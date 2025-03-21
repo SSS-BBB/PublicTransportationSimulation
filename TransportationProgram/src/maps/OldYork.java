590eb1828af602242d8e3a50f4f66b33d9860dad
Suphawit Buaphan <122143843+SSS-BBB@users.noreply.github.com>
2025-03-21 20:54:34 +0700

    fix typo

--- TransportationProgram/src/maps/OldYork.java
+++ TransportationProgram/src/maps/OldYork.java
@@ -21,10 +21,10 @@ public class OldYork extends MapData {
 		};
 		
 		StopSign[] vanStop = new StopSign[] { 
-				new StopSign("Trasport Garage", "A06", new double[] { 34.0/mapScale, 242.0/mapScale }),
+				new StopSign("Transport Garage", "A06", new double[] { 34.0/mapScale, 242.0/mapScale }),
 				new StopSign("Coco Industry", "A01", new double[] { 116.0/mapScale, 139.0/mapScale }),
 				new StopSign("Tiny Bangna", "A07", new double[] { 247.0/mapScale, 178.0/mapScale }),
-				new StopSign("Peerapol Staduium", "A08", new double[] { 333.0/mapScale, 126.0/mapScale }),
+				new StopSign("Peerapol Stadium", "A08", new double[] { 333.0/mapScale, 126.0/mapScale }),
 				new StopSign("Mee", "A09", new double[] { 429.0/mapScale, 74.0/mapScale }),
 				new StopSign("Bastoni's Park", "A10", new double[] { 518.0/mapScale, 103.0/mapScale }),
 				new StopSign("Poom Gaming", "A11", new double[] { 635.0/mapScale, 75.0/mapScale }),
@@ -36,14 +36,14 @@ public class OldYork extends MapData {
 				new TrainStopSign("39 Bridge", "A14", new double[] { 188.0/mapScale, 540.0/mapScale }),
 				new TrainStopSign("Warm Warm where you go last night", "A15", new double[] { 312.0/mapScale, 540.0/mapScale }),
 				new TrainStopSign("Mix Tong Gold shop", "A16", new double[] { 420.0/mapScale, 540.0/mapScale }),
-				new TrainStopSign("Tew strip club", "A17", new double[] { 535.0/mapScale, 540.0/mapScale }),
+				new TrainStopSign("Tew gaming club", "A17", new double[] { 535.0/mapScale, 540.0/mapScale }),
 				new TrainStopSign("Copter helicopter", "A18", new double[] { 643.0/mapScale, 540.0/mapScale }),
 				new TrainStopSign("BenzZaWorld", "A19", new double[] { 756.0/mapScale, 540.0/mapScale })
 		};
 		
 		StopSign[] skyTrainStop = new StopSign[] { 
 				new SkyTrainStopSign("Sky train Garage", "A20", new double[] { 102.0/mapScale, 356.0/mapScale }),
-				new SkyTrainStopSign("Yong Tui strip club", "A21", new double[] { 260.0/mapScale, 318.0/mapScale }),
+				new SkyTrainStopSign("Yong J tennis club", "A21", new double[] { 260.0/mapScale, 318.0/mapScale }),
 				new SkyTrainStopSign("Focus's Bar", "A22", new double[] { 441.0/mapScale, 307.0/mapScale }),
 				new SkyTrainStopSign("Niti 1st Racing Car", "A23", new double[] { 609.0/mapScale, 344.0/mapScale }),
 				new SkyTrainStopSign("Coconut Melon", "A24", new double[] { 714.0/mapScale, 424.0/mapScale }),
@@ -53,16 +53,16 @@ public class OldYork extends MapData {
 		};
 		
 		StopSign[] airConditionedBusStop = new StopSign[] {
-				new StopSign("Trasport Garage", "A06", new double[] { 34.0/mapScale, 242.0/mapScale }),
+				new StopSign("Transport Garage", "A06", new double[] { 34.0/mapScale, 242.0/mapScale }),
 				new StopSign("Tiny Bangna", "A07", new double[] { 247.0/mapScale, 178.0/mapScale }),
-				new StopSign("Peerapol Staduium", "A08", new double[] { 333.0/mapScale, 126.0/mapScale }),
+				new StopSign("Peerapol Stadium", "A08", new double[] { 333.0/mapScale, 126.0/mapScale }),
 				new StopSign("Asus Shop", "A28", new double[] { 575.0/mapScale, 260.0/mapScale }),
 				new StopSign("Moodeng", "A04", new double[] { 593.0/mapScale, 178.0/mapScale }),
 				new StopSign("Mee Mee Fountain", "A29", new double[] { 750.0/mapScale, 367.0/mapScale })
 		};
 		
 		StopSign[] smartBusStop = new StopSign[] {
-				new StopSign("Trasport Garage", "A06", new double[] { 34.0/mapScale, 242.0/mapScale }),
+				new StopSign("Transport Garage", "A06", new double[] { 34.0/mapScale, 242.0/mapScale }),
 				new StopSign("Tiny Bangna", "A07", new double[] { 247.0/mapScale, 178.0/mapScale }),
 				new StopSign("Pepe Shabu", "A03", new double[] { 391.0/mapScale, 206.0/mapScale }),
 				new StopSign("Asus Shop", "A28", new double[] { 590.0/mapScale, 209.0/mapScale }),
